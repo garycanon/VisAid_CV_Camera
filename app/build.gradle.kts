bcc077631026c07e3735284b0907d7ddcd9dbac7
Garreth Canon <garreth.canon@gmail.com>
2023-12-07 18:48:35 +0800

    chaquopy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,36 @@
 plugins {
     id("com.android.application")
+    id("com.chaquo.python")
 }
+
 
 android {
     namespace = "com.cpe4projdes1.visaid_cv_camera"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.cpe4projdes1.visaid_cv_camera"
-        minSdk = 21
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        ndk {
+            abiFilters "armeabi-v7a", "arm64-v8a", "x86", "x86_64"
+        }
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        python {
+            version "3.8"
+            buildPython "C:/path/to/python.exe"
+            buildPython "C:/path/to/py.exe", "-3.8"
+        }
+
+        buildTypes {
+            release {
+                minifyEnabled false
+                proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+            }
+        }
+
+        compileOptions {
+            sourceCompatibility JavaVersion.VERSION_1_8
+                    targetCompatibility JavaVersion.VERSION_1_8
+        }
+
     }
 
     buildTypes {
@@ -34,13 +51,14 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.camera:camera-camera2:1.1.0")
-    implementation("androidx.camera:camera-lifecycle:1.1.0")
-    implementation("androidx.camera:camera-view:1.1.0")
+    implementation("androidx.camera:camera-camera2:1.3.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.0")
+    implementation("androidx.camera:camera-view:1.3.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
